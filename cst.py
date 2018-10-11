669ce34874a04f573b6fa382716025401d72ec14
JP <9338170+u5a@users.noreply.github.com>
2018-10-11 23:41:53 +0200

    Added some code and test image

---
+++ cst.py
@@ -0,0 +1,25 @@
+from PIL import Image
+from PIL import ImageOps
+import pyscreenshot as ImageGrab #screengrab on Win/*nix, pillow won't do Linux
+import pytesseract
+import numpy as np
+import os
+
+#take screenshot and convert to grayscale
+#im = ImageGrab.grab(170, 670, 614, 960)
+
+#using screenshot for now, easier to test
+im = Image.open("./img/csgo-1.png").crop((170, 670, 614, 960))
+data = np.array(im)
+red, green, blue, alpha = data.T
+
+#take brighter px of image and convert dark pixels to black for better contrast
+dark_areas = (red <= 155) & (blue <= 155) & (green <= 155)
+data[..., :-1][dark_areas.T] = (0,0,0) #make dark areas black
+#gray = im.convert('1')
+#gray.show()
+
+im2 = Image.fromarray(data)
+im2.show()
+text = pytesseract.image_to_string(im2)
+print(text)
