a9c785826ac60cc1870c1ec11f06944d3b2c4da8
Joshua Green <joshuagreen2312@gmail.com>
2018-10-12 20:06:53 +0100

    moves research into main python file

--- cst.py
+++ cst.py
@@ -11,19 +11,24 @@ import cv2
 #crop values are the area of the screenshot where the text is (for now)
 #will have to be dynamic at some point due to different resolutions, 16x9, 4x3
 
-im = Image.open("./img/csgo-1.png").crop((170, 670, 614, 960))
-data = np.array(im)
-red, green, blue, alpha = data.T
+errosion_kernel = np.ones((2,1),np.uint8) #Kernel skewed high because text is usually longer than it is wide
+dilation_kernel = np.ones((6,5),np.uint8)
 
-im.show()
+crop_area = ((170, 800), (614, 1000)) # (x,y) pairs, 1st top left 2nd bottom right
 
-#take brighter px of image and convert dark pixels to black for better contrast
-dark_areas = (red <= 155) & (blue <= 155) & (green <= 155)
-data[..., :-1][dark_areas.T] = (0,0,0) #make dark areas black
-#gray = im.convert('1')
-#gray.show()
+img = cv2.imread('img/csgo-1.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray,1) 
+text_area = gray[crop_area[0][1]:crop_area[1][1], crop_area[0][0]:crop_area[1][0]] #numpy slice [y:y, x:x]
 
-im2 = Image.fromarray(data)
-im2.show()
-text = pytesseract.image_to_string(im2)
+thr3 = cv2.adaptiveThreshold(text_area,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,39,-145) # All values obtained via experimentation
+
+eroded = cv2.erode(thr3,errosion_kernel,iterations = 1)
+dilated = cv2.dilate(eroded,dilation_kernel,iterations = 3)
+
+Image.fromarray(dilated).show()
+extracted = cv2.bitwise_and(text_area,text_area, mask = dilated)
+Image.fromarray(extracted).show()
+
+text = pytesseract.image_to_string(extracted)
 print(text)
